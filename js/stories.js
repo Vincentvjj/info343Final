dd1ffb18fcb40822a593f8a7540d3b451fab57e2
vincent <joannb@uw.edu>
2014-12-01 15:02:52 -0800

    home done

--- js/stories.js
+++ js/stories.js
@@ -4,26 +4,26 @@
 
 "use strict";
 
-$(document).ready(function() {
-    var nav = $('nav');
-    var navTop = nav.offset().top;
-    var navHeight = nav.outerHeight();
-
-    var navPlaceHolder = $('.nav-placeholder');
-
-    navPlaceHolder.height(navHeight);
-
-    $(window).scroll(function() {
-        var scrollPos = $(this).scrollTop();
-        if (scrollPos > navTop) {
-            nav.addClass('nav-fixed');
-            navPlaceHolder.show();
-        }
-        else {
-            nav.removeClass('nav-fixed');
-            navPlaceHolder.hide();
-        }
-    });
-
-
-});
+//$(document).ready(function() {
+//    var nav = $('nav');
+//    var navTop = nav.offset().top;
+//    var navHeight = nav.outerHeight();
+//
+//    var navPlaceHolder = $('.nav-placeholder');
+//
+//    navPlaceHolder.height(navHeight);
+//
+//    $(window).scroll(function() {
+//        var scrollPos = $(this).scrollTop();
+//        if (scrollPos > navTop) {
+//            nav.addClass('nav-fixed');
+//            navPlaceHolder.show();
+//        }
+//        else {
+//            nav.removeClass('nav-fixed');
+//            navPlaceHolder.hide();
+//        }
+//    });
+//
+//
+//});
