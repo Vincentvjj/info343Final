6bd4204f5399e098355abc5a1781a59b01ee4c56
vincent <jonanv@uw.edu>
2014-12-01 19:39:18 -0800

    a

--- js/app.js
+++ js/app.js
@@ -90,44 +90,44 @@ $(document).ready(function() {
 
 
 
-    $('#submit-button').click(submitEmail);
+    // $('#submit-button').click(submitEmail);
 
-        function submitEmail(event) {
-            event.preventDefault();
-            // email info here!
-            var data = {
-                from: $('#submitForm input[name="email"]').val(),
-                subject: $('#submitForm input[name="subject"]').val(),
-                text: $('#submitForm input[name="message"]').val()
-            };
+    //     function submitEmail(event) {
+    //         event.preventDefault();
+    //         // email info here!
+    //         var data = {
+    //             from: $('#submitForm input[name="email"]').val(),
+    //             subject: $('#submitForm input[name="subject"]').val(),
+    //             text: $('#submitForm input[name="message"]').val()
+    //         };
 
-            $.ajax('api/send', {
-                'data': data,
-                success: submitted,
-                error: failed
-            })
-        }
+    //         $.ajax('api/send', {
+    //             'data': data,
+    //             success: submitted,
+    //             error: failed
+    //         })
+    //     }
 
-        function submitted() {
-            $('#submitForm input').each(function(index, value) {
-                $(this).val('');
-            });
+    //     function submitted() {
+    //         $('#submitForm input').each(function(index, value) {
+    //             $(this).val('');
+    //         });
 
-            $('#successalert').slideDown(400, function() {
-                window.setTimeout(function() {
-                    $('#successalert').slideUp();
-                }, 2500)
-            });
-        }
+    //         $('#successalert').slideDown(400, function() {
+    //             window.setTimeout(function() {
+    //                 $('#successalert').slideUp();
+    //             }, 2500)
+    //         });
+    //     }
 
-        // shows the error thing for a bit
-        function failed() {
-            $('#erroralert').slideDown(400, function() {
-                window.setTimeout(function() {
-                    $('#erroralert').slideUp();
-                }, 2500)
-            });
-        }
+    //     // shows the error thing for a bit
+    //     function failed() {
+    //         $('#erroralert').slideDown(400, function() {
+    //             window.setTimeout(function() {
+    //                 $('#erroralert').slideUp();
+    //             }, 2500)
+    //         });
+    //     }
 
 
     
