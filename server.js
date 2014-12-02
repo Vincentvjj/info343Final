e659a184e400da99a1dbbc6325814b511044f9fa
vincent <jonanv@uw.edu>
2014-12-01 19:16:26 -0800

    added server

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
 
 
 //create an express application
@@ -29,20 +30,19 @@ app.use(function(err, req, res, next) {
 });
 
 app.get('/api/send', function (req, res){
-    // do something here
-    // sendgrid.send({
-    //     to: req.query.to,
-    //     from: 'info343@ischool.uw.edu',
-    //     subject: req.query.subject,
-    //     text: req.query.text
-    // }, function(err, json) {
-    //     if(err) {
-    //         res.status(500).send('internal serve error!');
-    //     }
-    //     else {
-    //         res.status(200).send('everything went well, email sent!');
-    //     }
-    // });
+    sendgrid.send({
+        to: 'vinc3nt_joe@yahoo.com',
+        from: req.query.to,
+        subject: req.query.subject,
+        text: req.query.text
+    }, function(err, json) {
+        if(err) {
+            res.status(500).send('internal serve error!');
+        }
+        else {
+            res.status(200).send('everything went well, email sent!');
+        }
+    });
 });
 
 //start the web server
