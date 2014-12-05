8d478f9d0148f4b33e1b2d69131a04a85e441cad
vincent <jonanv@uw.edu>
2014-12-04 18:40:45 -0800

    final

--- static/js/app.js
+++ static/js/app.js
@@ -15,66 +15,64 @@ $(document).ready(function() {
     var slideSwitch = $("[name='my-checkbox']");
     navPlaceHolder.height(navHeight);
 
-    var angle = 0;  
+    var angle = 0;
 
 
     $('#carousel').bind('keypress', function(e) {
         var code = e.keyCode || e.which;
-        if(code == 37) { //Enter keycode
-           angle = angle - 45; 
-           $('#spinner').css('transform', "rotateY("+ angle +"deg)");
+        if (code == 37) { //Enter keycode
+            angle = angle - 45;
+            $('#spinner').css('transform', "rotateY(" + angle + "deg)");
         }
-        if(code == 39) {
-            angle = angle + 45; 
-            $('#spinner').css('transform', "rotateY("+ angle +"deg)");
+        if (code == 39) {
+            angle = angle + 45;
+            $('#spinner').css('transform', "rotateY(" + angle + "deg)");
         }
     });
 
     $('#right').click(function() {
-        angle = angle + 45; 
-        $('#spinner').css('transform', "rotateY("+ angle +"deg)");
+        angle = angle + 45;
+        $('#spinner').css('transform', "rotateY(" + angle + "deg)");
     });
 
     $('#left').click(function() {
-        angle = angle - 45; 
-        $('#spinner').css('transform', "rotateY("+ angle +"deg)");
+        angle = angle - 45;
+        $('#spinner').css('transform', "rotateY(" + angle + "deg)");
     });
 
 
-    if(document.getElementById("contact-form")) {
+    if (document.getElementById("contact-form")) {
         document.getElementById("contact-form").addEventListener("submit", onSubmit);
-    }   
+    }
 
-    if($("p a[href='#top']")) {
+    if ($("p a[href='#top']")) {
         $('p a[href!="#top"]').attr('target', '_blank');
         $("p a[href='#top']").click(function(eventObject) {
             var targetElement = $(this.hash);
             $('html, body').animate({
                 scrollTop: targetElement.offset().top - navHeight
             }, 700);
-            eventObject.preventDefault(); 
+            eventObject.preventDefault();
         });
 
     } else {
-        //return
+        return;
     }
 
-    if(slideSwitch.length == 0) {
-        //return
+    if (slideSwitch.length == 0) {
+        return;
     } else {
         slideSwitch.bootstrapSwitch('onText', 'English');
         slideSwitch.bootstrapSwitch('offText', 'Spanish');
         slideSwitch.bootstrapSwitch('labelText', 'Spanish&#8594;');
 
         slideSwitch.on('switchChange.bootstrapSwitch', function(event, state) {
-            if(state) {
+            if (state) {
                 $('#english-lang').css('display', 'block');
                 $('#spanish-lang').css('display', 'none');
                 slideSwitch.bootstrapSwitch('labelText', 'Spanish&#8594');
                 //show english
-            }
-
-            else {
+            } else {
                 $('#english-lang').css('display', 'none');
                 $('#spanish-lang').css('display', 'block');
                 slideSwitch.bootstrapSwitch('labelText', '&#8592English');
@@ -86,51 +84,49 @@ $(document).ready(function() {
 
     $('#submit-button').click(submitEmail);
 
-        function submitEmail(event) {
-            event.preventDefault();
-            // email info here!
-            var data = {
-                from: $('#contact-form input[name="email"]').val(),
-                subject: $('#contact-form input[name="subject"]').val(),
-                text: $('#message-input').val() + '\n' + '\n' + '--Sent from Fotohistorias--'
-            };
+    function submitEmail(event) {
+        event.preventDefault();
+        // email info here!
+        var data = {
+            from: $('#contact-form input[name="email"]').val(),
+            subject: $('#contact-form input[name="subject"]').val(),
+            text: $('#message-input').val() + '\n' + '\n' + '--Sent from Fotohistorias--'
+        };
 
-            $.ajax('api/send', {
-                'data': data,
-                success: submitted,
-                error: failed
-            })
-        }
+        $.ajax('api/send', {
+            'data': data,
+            success: submitted,
+            error: failed
+        })
+    }
 
-        function submitted() {
-            $('#contact-form input').each(function(index, value) {
-                $(this).val('');
-                $('#message-input').val('');
-            });
+    function submitted() {
+        $('#contact-form input').each(function(index, value) {
+            $(this).val('');
+            $('#message-input').val('');
+        });
 
-            $('#successalert').slideDown(400, function() {
-                window.setTimeout(function() {
-                    $('#successalert').slideUp();
-                }, 2500)
-            });
-        }
+        $('#successalert').slideDown(400, function() {
+            window.setTimeout(function() {
+                $('#successalert').slideUp();
+            }, 2500)
+        });
+    }
 
-        // shows the error thing for a bit
-        function failed() {
-            $('#erroralert').slideDown(400, function() {
-                window.setTimeout(function() {
-                    $('#erroralert').slideUp();
-                }, 2500)
-            });
-        }
+    // shows the error thing for a bit
+    function failed() {
+        $('#erroralert').slideDown(400, function() {
+            window.setTimeout(function() {
+                $('#erroralert').slideUp();
+            }, 2500)
+        });
+    }
 });
-
-
 
 function onSubmit(evt) {
     evt.returnValue = validateForm(this);
     if (evt.returnValue == false && evt.preventDefault) {
-    evt.preventDefault();
+        evt.preventDefault();
     }
 
     return evt.returnValue;
@@ -141,34 +137,22 @@ function validateForm(form) {
     var requiredFields = ['email', 'subject', 'message'];
     var formValid = true;
 
-    for(var i = 0; i < requiredFields.length; i++) {
+    for (var i = 0; i < requiredFields.length; i++) {
         formValid &= validateRequiredField(form.elements[requiredFields[i]]);
     }
 
-
     return formValid;
-} 
+}
 
 function validateRequiredField(field) {
     var value = field.value.trim();
     var valid = value.length > 0;
-    if(valid) {
+    if (valid) {
         field.className = "form-control";
 
-    }
-
-    else {
+    } else {
         field.className = "form-control invalid-field";
     }
 
     return valid;
-
-
 }
-//
-//$('.carousel').carousel({
-//    interval: 3000
-//});
-//
-
-
